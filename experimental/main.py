dd647a773262fd898f649a5c38814fbd9394b845
Live session user <Harrison Walker>
2020-09-12 23:35:34 -0400

    added first rough OOP files, now obscelete, to folder called experimental

---
+++ experimental/main.py
@@ -0,0 +1,15 @@
+import csv
+from sensor import Sensor
+
+# with open('SensorConfig.csv') as csvfile:
+#     reader = csv.reader(csvfile)
+#     for row in reader:
+#         print (', '.join(row))
+
+
+with open('SensorConfig.csv') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        for item in row:
+            
+        #create Sensor object
