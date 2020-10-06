52c0a35e27050da04aca2c1ed6ea0074b2940bcb
irwinfrimpong <frimponi@lafayette.edu>
2020-10-06 15:01:14 -0400

    Wrire method for i2c

--- i2c_sorter_test.py
+++ i2c_sorter_test.py
@@ -4,18 +4,23 @@ import time
 
 bus = smbus.SMBus(1)
 
-reg_address = [0xFB,0xFA] #Temprature
+reg_address = [0xF4 0xF5] #Temprature
 address = 0x77
+Value = 9988
 
 while True:
     
-    try:
-        data = 0
-        for i in range(len(reg_address)):
-            data = data + bus.read_byte_data(address, reg_address[i]) << (8 * i)
-            # data = str(bus.read_byte(address,reg_address[i]) << (8 * i))+data
-        print(data)
-        time.sleep(1)
+    
+        try:
+            #Take out in future if not need 2 reg to write to
+            for i in range(len(str(hex(Value)).replace("0x",""))-1):
+                #bus.write_byte(Sensor.reg_address[i/2],str(hex(Value))[i-1]+str(hex(Value))[i])
+                bus.write_byte_data(Sensor.reg_address[i],hex(((0xFF << i*8)&Value)>>i*8))
+                # print(str(hex(Value))[i-1]+str(hex(Value))[i])
+        
+        print("Expected Data: " + str(bus.read_byte_data(0xF5)) + str(bus.read_byte_data(0xF4))
+                    
+        
 
         #reg_address = [0x03 ,0x04, 0x05]
         #address = 0x68
