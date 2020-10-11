304653323af34beaa855c2f6bd46219a9c404886
irwinfrimpong <frimponi@lafayette.edu>
2020-10-10 20:09:48 -0400

    Working on redis

--- Driver.py
+++ Driver.py
@@ -1,23 +1,25 @@
-
 import sys, os
 import utils
-import usr/etc/scada/config
+#import usr/etc/scada/config
 
 class Driver:
     def __init__():
 
+Redisdata = redis.Redis(host='localhost', port=6379, db=0)
 
  # Method to read from the sesnor objects depending on protocol                
-    def read( Sensor)
+    def read(Sensor)
     #make it look at the folder for what protocol to use
         if(Sensor.protocol == 'I2C'):
-            i2c_sorter.read(Sensor)
+            data = i2c_sorter.read(Sensor)
         elif(Sensor.protocol =='CAN'):
-            can_sorter.read(Sensor)
+            data = can_sorter.read(Sensor)
         elif(Sensor.protocol == 'USB'):
-            usb_sorter.read(Sensor)
+            data= usb_sorter.read(Sensor)
         else:
             return 'Sensor Protocol Not Found'
+         #Redis Write Command 
+        Redisdata.publish('data', data)
             
     #Write to sensor 
     def write(Sensor,Value)
--- i2c_sorter.py
+++ i2c_sorter.py
@@ -10,12 +10,14 @@ class i2c_sorter:
     bus = smbus2.SMBUS(1)
     def read(Sensor):
         try:
+            #reads from the rtc if the address matches
             if(Sensor.address == 0x68):
                 return read_rtc(Sensor)
             else:
-                data = ''
-                for i in range(len(Sensor.address)):
-                    data = str(bus.read_byte(Sensor.address,Sensor.reg_address[i]) << (8 * i-1))+data
+                data = 0
+                #adds the values for each byte of the sensor together to get the overall result of the sensor
+                for i in range(len(Sensor.reg_address)):
+                    data = data + bus.read_byte_data(Sensor.address,Sensor.reg_address[i]) << (8 * i)
                 return data
         except IOError:
             time.sleep(.0001)
@@ -23,20 +25,23 @@ class i2c_sorter:
 
     def write(Sensor, Value):
         try:
-            for i in range(len(str(hex(Value)).replace("0x",""))-1):
-                if(i % 2 == 0):
-                    #bus.write_byte(Sensor.reg_address[i/2],str(hex(Value))[i-1]+str(hex(Value))[i])
-                    bus.write_byte(Sensor.reg_address[i/2],hex(((0xFF << i*4)&Value)>>i*4))
-                    # print(str(hex(Value))[i-1]+str(hex(Value))[i])
-                    
+            if(Sensor.address == 0x68):
+                return write_rtc(Sensor,Value)
+            else:
+            #Take out in future if not need 2 reg to write to
+                bus.write_byte_data(Sensor.address,Sensor.reg_address,Value)
+                # print(str(hex(Value))[i-1]+str(hex(Value))[i])  
         except IOError:
             time.sleep(.0001)
-
+    
+#Read function for RTC pcf-8523
     def read_rtc(Sensor)
         data = ""
         seconds_data = ""
         mins_data = ""
         hours_data= ""
+
+    try:
         for i in range(len(Sensor.reg_address)):
             busval = bus.read_byte_data(Sensor.address,Sensor.reg_address[i])
             if (i == 0):
@@ -45,8 +50,39 @@ class i2c_sorter:
                 mins_data = str(hex(((busval & 0xF0)>> 4))) + str(hex((busval & 0xF)))
             if (i == 2):
                 hours_data = str(hex(((busval & 0xF0)>> 4))) + str(hex((busval & 0xF)))
+            if (i == 3):
+                days_data = str(hex(((busval & 0xF0)>> 4))) + str(hex((busval & 0xF))) 
+            if (i == 4):
+                months_data = str(hex(((busval & 0xF0)>> 4))) + str(hex((busval & 0xF)))
+            if (i == 5):
+                years_data = str(hex(((busval & 0xF0)>> 4))) + str(hex((busval & 0xF)))
+            
         #Save to redis
-        return (hours_data + ":" + mins_data + ":" + seconds_data).replace("0x","")
+        return (years_data + ":" + months_data + ":" + days_data + ":" + hours_data + ":" + mins_data + ":" + seconds_data).replace("0x","")
+    
+    except IOError:
+            time.sleep(.0001)
+
+
+    #Write Function for RTC to set date and time
+    def write_rtc(Sensor,Value)
+     # 'YR:MO:DD:HR:MI:SS' How we want value to be inputted
+     val=Value.split(":")
+        print(val)
+    try:
+        bus.write_byte_data(0x68,Sensor.reg_address[0],int(val[0],16)) #Year
+        bus.write_byte_data(0x68,Sensor.reg_address[1],int(val[1],16))
+        bus.write_byte_data(0x68,Sensor.reg_address[2],int(val[2],16))
+        bus.write_byte_data(0x68,Sensor.reg_address[3],int(val[3],16))
+        bus.write_byte_data(0x68,Sensor.reg_address[4],int(val[4],16))
+        bus.write_byte_data(0x68,Sensor.reg_address[5],int(val[5],16))
+
+        ##Conguring the Date
+
+
+    except IOError:
+            time.sleep(.0001)
+    
 
     
         
