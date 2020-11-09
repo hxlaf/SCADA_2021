cefe18e5a8ab6a62ca289825d0d344462d02c637
Harrison Walker <walkerhd@lafayette.edu>
2020-11-09 15:00:47 -0500

    cleaned some stuff up in config

--- utils/database.py
+++ utils/database.py
@@ -21,7 +21,7 @@ def getData(sensor_id):
         if it exists. To be used by other classes to retreive information
         from database.
     """
-    print(sensor_id)
+    #print(sensor_id)
     cursor.execute("""
         SELECT value, timestamp
         FROM data
