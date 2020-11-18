9a65af45551d3ab9eaefe617694f2fd8153b421e
irwinfrimpong <frimponi@lafayette.edu>
2020-11-17 23:32:07 -0500

    Code to read IMU registers

--- smbus2_imutest.py
+++ smbus2_imutest.py
@@ -1,24 +1,18 @@
 import smbus2
-import time
+# import time
 # Open i2c bus 1 and read one byte from address 80, offset 0
 
-bus = smbus2.SMBus(1)
-c= 1
-while True:
+bus = smbus.SMBus(1)
+c={ "Chip Id":0x00,"Temp":0x34,"Calibration Status":0x35,"Sys Status":0x39,"ST Result":0x36,"Sys_Error":0x3a,"Unit Selection":0x3b,"Operation Mode":0x3d,
+"Power Mode":0x3e,"Temp Source":0x40}
+
+for key in c :
     try:
-        a = bus.read_byte_data(0x77, 0x88, force=None)
-        b = bus.read_byte_data(0x77, 0x89, force = None)
-        print("Temp" ,(a<<8)+b, " a: ", a, " b: ", b, end = '\r')
+        a = bus.read_byte_data(0x28, c.get(key), force=None)
+        print(key + ": " +  str(a))
         time.sleep(0.25)
-        
-        #print("After Sleep", c)
-        #c=c+1
+       
     except IOError:
         time.sleep(.002)
-# c= bus.read_byte_data(0x77, 0xF8, force=None)
-# print("LSB",c)
-# #
-# #val = (b << 8) + c 
-# 
-# #print("Final Val: " ,val)
+
 bus.close()
