94a954eef6813329704d328e9e7dbc9ab1990e2c
irwinfrimpong <frimponi@lafayette.edu>
2021-01-09 22:18:42 -0500

    Editing the imu setup method

--- sorter/sorter_new.py
+++ sorter/sorter_new.py
@@ -46,8 +46,9 @@ def imu_reset():
 
 def imu_setup():
     #Debuggin: 
-    print("Value of Opr_Mode: " + driver.read('opr_mode_reg') )
-    if (driver.read('opr_mode_reg') == 0 ):
+    opr_mode_reg_read = driver.read('opr_mode_reg')
+    print("Value of Opr_Mode: " + opr_mode_reg_read )
+    if (opr_mode_reg_read == 0 and opr_mode_reg_read != 12): #If its in Config Mode and not in NDOF mode, want to configure it
         imu_reset()
         driver.write('power_reg',config.get('IMU_Config_Constants').get('POWER_NORMAL'))
         driver.write('page_reg',0x00)
