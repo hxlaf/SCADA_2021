9eeaaee3f572b64b286818990c5cc8436de9ed25
irwinfrimpong <frimponi@lafayette.edu>
2021-01-16 14:01:15 -0500

    Offset registers for imu added to be displayed

--- sorter/sorter_new.py
+++ sorter/sorter_new.py
@@ -49,14 +49,15 @@ def imu_setup():
     opr_mode_reg_read = driver.read('opr_mode_reg')
     print("Value of Opr_Mode: " + str(opr_mode_reg_read))
     print( "SensorList Dictionary: " + str(SensorList))
-    if (opr_mode_reg_read == 0 or opr_mode_reg_read != 12): #If its in Config Mode and not in NDOF mode, want to configure it
+   # if (opr_mode_reg_read == 0 or opr_mode_reg_read != 12): #If its in Config Mode and not in NDOF mode, want to configure it
+    if (opr_mode_reg_read == 0 ):
         imu_reset()
         driver.write('power_reg',config.get('IMU_Config_Constants').get('POWER_NORMAL'))
         driver.write('page_reg',0x00)
         driver.write('trigger_reg',0x00)
         driver.write('acc_config_reg',config.get('IMU_Config_Constants').get('ACCEL_4G'))
-        driver.write('gyro_config_reg',config.get('IMU_Config_Constants').get('GYRO_2000_DPS'))
-        driver.write('mag_config_reg',config.get('IMU_Config_Constants').get('MAGNETOMETER_20HZ'))
+        #driver.write('gyro_config_reg',config.get('IMU_Config_Constants').get('GYRO_2000_DPS'))
+        #driver.write('mag_config_reg',config.get('IMU_Config_Constants').get('MAGNETOMETER_20HZ'))
         time.sleep(0.01)
     
         ##Setting IMU TO NDOF MODE
