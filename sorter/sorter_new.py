c9e15b1d6fcf8dc3dcfc3a995e8201f7089c4a59
irwinfrimpong <frimponi@lafayette.edu>
2021-01-17 21:54:35 -0500

    scope of onSetup

--- sorter/sorter_new.py
+++ sorter/sorter_new.py
@@ -32,6 +32,7 @@ for key in config.get('Sensors'):
     last_sampled[key] = time.time()
 
 ####### Methods to Configure BNO055 IMU #######
+global onSetup
 onSetup = False  #Boolean var used to peform imu setup on startup
 
 def imu_reset():
@@ -52,7 +53,7 @@ def imu_setup():
     print( "SensorList Dictionary: " + str(SensorList))
    # if (opr_mode_reg_read == 0 or opr_mode_reg_read != 12): #If its in Config Mode and not in NDOF mode, want to configure it
     if (opr_mode_reg_read == 0 or (bool(onSetup) == False)):
-        global onSetup = True #OnSetup has been achieved 
+        onSetup = True #OnSetup has been achieved 
         imu_reset()
         driver.write('power_reg',config.get('IMU_Config_Constants').get('POWER_NORMAL'))
         driver.write('page_reg',0x00)
