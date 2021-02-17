f3457cc578e6b052354b74dde9afe327ae56866c
chrysanv <chrysanv@lafayette.edu>
2021-02-17 15:42:34 -0500

    unit bug fix

--- GUI/Anotha_one.py
+++ GUI/Anotha_one.py
@@ -203,8 +203,10 @@ class NewGUI_2(tk.Frame):
 
             ## add if != "Buss Error"
             #if value != "buss error":
-            
-            if sensor.get('unit') is None: 
+
+            if value = "NO DATA":
+                unit = " "
+            else if sensor.get('unit') is None: 
                 unit = " "
             else:
                 unit = sensor.get('unit')
