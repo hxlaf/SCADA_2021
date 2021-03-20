d94497fa46038f9f610ba52ead7b48848c0e192f
Harrison <walkerhd@lafayette.edu>
2021-03-20 17:49:37 -0400

    making test file for new CAN nodes

---
+++ drivers/can_scan.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+import sys, os
+
+#Importing the config file
+lib_path = '/usr/etc/scada'
+config_path = '/usr/etc/scada/config'
+#this is temporary, just for testing
+local_path = '../utils'
+
+sys.path.append(lib_path)
+sys.path.append(config_path)
+sys.path.append(local_path)
+
+from drivers import can_driver
+
+cd = can_driver.CanDriver()
+network = cd.network
+
+#ensure nodes are connected
+network.scanner.search()
+time.sleep(1)
+print("Connected Nodes:")
+for node_id in network.scanner.nodes:
+    print("Found node %d!" % node_id)
