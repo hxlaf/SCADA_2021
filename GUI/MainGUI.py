bdc7357744123dccd45c29b74c24c4296e9ad2a1
vasschrys <liachrys1@gmail.com>
2021-03-21 19:17:24 -0400

    adding background color

--- GUI/MainGUI.py
+++ GUI/MainGUI.py
@@ -73,7 +73,7 @@ class Main_GUI(tk.Tk):
            
             self.frames[i] = frame
             frame.grid(row=0, column=0, sticky="nsew")
-            frame.configure(bg = 'red')
+            #frame.configure(bg = 'red') #  set background color 
             self.display_vars["frames"][i] = self.frames[i]
 
 
